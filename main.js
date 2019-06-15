bbcff1c2bcf8953a16599520e39cf575d8346dad
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-06-15 10:44:52 +0800

    更改 JS API

--- main.js
+++ main.js
@@ -6,58 +6,60 @@ for (let i = 0; i < $slide.length; i++) {
         $('#images').css({
             transform: 'translateX(' + s1 + 'px)'
         })
+        $($slide[i]).addClass('active').siblings('.active').removeClass('active');
     })
     n = i;
 }
+
 // 自动轮播
 var n = 0;
-var timer = setInterval(function () {
-    n++;
-    if (n < $slide.length) {
-        let s2 = n * -920;
-        $('#images').css({
-            transform: 'translateX(' + s2 + 'px)'
-        })
-    } else {
-        n = -1;
-    }
-}, 2000)
+var $size = $slide.length;
+$slide.eq(n % $size).trigger('click')
+var timeID = timer();
 
 // 鼠标移入移出事件
-$('#wrapper').on('mouseenter', function () {
-    window.clearInterval(timer);
+$('.wrapper').on('mouseenter', function () {
+    window.clearInterval(timeID);
 })
-$('#wrapper').on('mouseleave', function () {
-    timer = setInterval(function () {
-        n++;
-        if (n < $slide.length) {
-            let s2 = n * -920;
-            $('#images').css({
-                transform: 'translateX(' + s2 + 'px)'
-            })
-        } else {
-            n = -1;
-        }
-    }, 2000)
+$('.wrapper').on('mouseleave', function () {
+    timeID = timer();
 })
+
 // 监听是否离开当前页面
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        window.clearInterval(timer);
+        window.clearInterval(timeID);
     } else {
-        timer = setInterval(function () {
-            n++;
-            if (n < $slide.length) {
-                let s2 = n * -920;
-                $('#images').css({
-                    transform: 'translateX(' + s2 + 'px)'
-                })
-            } else {
-                n = -1;
-            }
-        }, 2000)
+        timeID = timer();
     }
-
 })
+
+// 轮播计时器   
+function timer() {
+    return setInterval(() => {
+        n += 1;
+        $slide.eq(n % $size).trigger('click')
+    }, 2000)
+}
+
+// 监听是否离开当前页面
+// document.addEventListener('visibilitychange', function () {
+//     if (document.hidden) {
+//         window.clearInterval(timer);
+//     } else {
+//         timer = setInterval(function () {
+//             n++;
+//             if (n < $slide.length) {
+//                 let s2 = n * -920;
+//                 $('#images').css({
+//                     transform: 'translateX(' + s2 + 'px)'
+//                 })
+//             } else {
+//                 n = -1;
+//             }
+//         }, 2000)
+//     }
+
+// })
 
 
